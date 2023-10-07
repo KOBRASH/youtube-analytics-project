dbf1a2958e265f1c71203e8283750d2dd611ee4a
Sergei Aslanov <courageous@bk.ru>
2023-10-07 16:48:43 +0200

    Переделал модуль playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,14 +1,44 @@
 import datetime
+from googleapiclient.discovery import build
+
 
 # Базовый класс, содержащий общие атрибуты и метод show_best_video()
 class PlaylistBase:
-    def __init__(self, title, url):
-        self.title = title
-        self.url = url
-        self.videos = []  # Список видео в плейлисте
+    def __init__(self, playlist_id, api_key):
+        self.playlist_id = playlist_id
+        self.api_key = api_key
+        self.videos = self._get_playlist_videos()
 
-    def add_video(self, video):
-        self.videos.append(video)
+    def _get_playlist_videos(self):
+        # Создайте объект YouTube API
+        youtube_service = build('youtube', 'v3', developerKey=self.api_key)
+
+        # Запрос информации о видео в плейлисте
+        playlist_videos = youtube_service.playlistItems().list(
+            playlistId=self.playlist_id,
+            part='contentDetails',
+            maxResults=50,
+        ).execute()
+
+        # Получите все ID видеороликов из плейлиста
+        video_ids = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+
+        # Получите данные по каждому видео
+        video_response = youtube_service.videos().list(
+            part='contentDetails,statistics',
+            id=','.join(video_ids)
+        ).execute()
+
+        videos = []
+        for video_item in video_response.get('items', []):
+            video = {
+                'url': f'https://www.youtube.com/watch?v={video_item["id"]}',
+                'duration': video_item['contentDetails']['duration'],
+                'likes': int(video_item['statistics']['likeCount'])
+            }
+            videos.append(video)
+
+        return videos
 
     def show_best_video(self):
         if not self.videos:
@@ -18,38 +48,34 @@ class PlaylistBase:
         best_video = max(self.videos, key=lambda video: video['likes'])
         return best_video['url']
 
+
 # Миксин для вычисления суммарной длительности
 class DurationMixin:
     def total_duration(self):
         total_duration = datetime.timedelta()
         for video in self.videos:
-            duration_str = video.get('duration', '0:00:00')
-            hours, minutes, seconds = map(int, duration_str.split(':'))
-            video_duration = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
-            total_duration += video_duration
+            duration_str = video.get('duration', 'PT0S')  # PT0S - длительность по умолчанию (0 секунд)
+            duration = datetime.datetime.strptime(duration_str, "PT%HH%MM%SS")
+            total_duration += datetime.timedelta(
+                hours=duration.hour,
+                minutes=duration.minute,
+                seconds=duration.second
+            )
         return total_duration
+
 
 # Класс PlayList, который наследуется от PlaylistBase и DurationMixin
 class PlayList(PlaylistBase, DurationMixin):
-    def __init__(self, id):
-        title = "Moscow Python Meetup №81"
-        url = "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
-        super().__init__(title, url)
+    def __init__(self, playlist_id, api_key):
+        super().__init__(playlist_id, api_key)
+
 
 # Пример использования класса PlayList
 if __name__ == '__main__':
-    pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
-    # Добавим видео с информацией о длительности и лайках
-    pl.add_video({'url': 'https://youtu.be/cUGyMzWQcGM', 'duration': '0:25:30', 'likes': 1000})
-    pl.add_video({'url': 'https://youtu.be/abc123', 'duration': '0:30:45', 'likes': 1500})
-    pl.add_video({'url': 'https://youtu.be/xyz456', 'duration': '0:20:15', 'likes': 800})
+    playlist_id = 'PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw'
+    api_key = 'AIzaSyCmeF176p_td6JljSSx-ziWq63dZrnGdkg'
 
-    assert pl.title == "Moscow Python Meetup №81"
-    assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
+    pl = PlayList(playlist_id, api_key)
 
-    duration = pl.total_duration()
-    assert str(duration) == "1:16:30"
-    assert isinstance(duration, datetime.timedelta)
-    assert duration.total_seconds() == 4590.0
-
-    assert pl.show_best_video() == "https://youtu.be/abc123"  # Самое популярное видео
+    print(f"Best Video URL: {pl.show_best_video()}")  # Самое популярное видео
+    print(f"Total Duration: {pl.total_duration()}")
